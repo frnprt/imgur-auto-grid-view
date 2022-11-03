94506560ab692af2ca809ba94a32fbeea728b473
frnprt <44530535+frnprt@users.noreply.github.com>
2022-11-03 19:23:35 +0100

    Fix minor tabulations typos

--- imgur-auto-grid-view.user.js
+++ imgur-auto-grid-view.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
-// @name		ImgurAutoGrid-View
+// @name        ImgurAutoGrid-View
 // @namespace   https://github.com/frnprt/ImgurAutoGrid-View
 // @description Automatically show albums in grid view
-// @match		http://imgur.com/a/*
-// @match		https://imgur.com/a/*
-// @version		1.0
+// @match       http://imgur.com/a/*
+// @match       https://imgur.com/a/*
+// @version     1.0
 // @updateURL   https://github.com/frnprt/ImgurAutoGrid-View/raw/main/imgur-auto-grid-view.user.js
 // dowloadURL   https://github.com/frnprt/ImgurAutoGrid-View/raw/main/imgur-auto-grid-view.user.js
-// @author		frnprt
-// @grant		none
+// @author      frnprt
+// @grant       none
 // ==/UserScript==
 
 (function(){
